b45e95c276351cab9e4b3c41c4921cdfbae1766b
rameshdhage <dhageramesh143@gmail.com>
2024-02-07 17:05:05 +0530

    find the sum of the even number by using java 1.8 features

---
+++ src/com/demo/FindTheSumOfEvenNumber.java
@@ -0,0 +1,17 @@
+package com.demo;
+
+import java.util.Arrays;
+
+//using java 1.8 feature
+public class FindTheSumOfEvenNumber {
+	
+	public static void main(String[] args) {
+		
+		int [] numbers= {1,2,3,4,5,6,7,8,9,10};
+		
+		int sum=Arrays.stream(numbers).filter(n->n%2==0).sum();
+		
+		System.out.println("The sumof the even number is:"+sum);
+	}
+
+}
