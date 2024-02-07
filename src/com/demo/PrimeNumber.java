533568ab0527eb208398e5642f68a0976986d7f5
rameshdhage <dhageramesh143@gmail.com>
2024-02-07 16:02:06 +0530

    add the class for prime number and also logic

---
+++ src/com/demo/PrimeNumber.java
@@ -0,0 +1,27 @@
+package com.demo;
+
+public class PrimeNumber {
+	
+	public static void main(String[] args) {
+		
+		int num=7;
+		int a=0;
+		
+		for(int i=2;i<num-1;i++) {
+			if(num%2==0) {
+				a=a+1;
+				
+			}}
+			if(a==0) {
+				System.out.println("This is a prime number="+num);
+				
+			}
+			else {
+				System.out.println("This is a not prime number="+num);
+			}
+		}
+		
+	
+	
+
+}
