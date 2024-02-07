42bcf631c24c3bbb4e3cad089d684b1d8a7226b5
rameshdhage <dhageramesh143@gmail.com>
2024-02-07 16:08:13 +0530

    added one class evenAnd or odd number also logic

---
+++ src/com/demo/EvenOddNumber.java
@@ -0,0 +1,18 @@
+package com.demo;
+
+public class EvenOddNumber {
+	
+	
+	public static void main(String args[]) {
+		int num=23;
+		
+		if(num%2==0) {
+			System.out.println("This number is a even number="+num);
+		}
+		System.out.println("This number is a odd number="+num);
+		
+	}
+	
+	
+
+}
